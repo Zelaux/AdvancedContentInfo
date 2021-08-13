c6a2d445080984a2b01d38f35992771826939a2c
Zelaux <ura20060605@gmail.com>
2021-08-13 14:08:45 +0300

    Added support for custom units of measurement for stat (use AStatValues for comfortable use)

---
+++ core/src/acontent/world/meta/AStatUnit.java
@@ -0,0 +1,43 @@
+package acontent.world.meta;
+
+import arc.Core;
+import arc.struct.ObjectMap;
+import mindustry.world.meta.StatCat;
+import mindustry.world.meta.StatUnit;
+
+import java.util.Locale;
+
+public class AStatUnit {
+    private static final ObjectMap<String, AStatUnit> staUnitMap = new ObjectMap<>();
+    static {
+        for (StatUnit value : StatUnit.values()) {
+            get(value);
+        }
+    }
+    public final boolean space;
+    private final String name;
+
+    public String name() {
+        return name;
+    }
+
+    public static AStatUnit get(StatUnit category) {
+        return get(category.name());
+    }
+    public static AStatUnit get(String name) {
+        return staUnitMap.get(name.toLowerCase(Locale.ROOT), () -> new AStatUnit(name));
+    }
+    private AStatUnit(String name,boolean space){
+        this.space = space;
+        this.name=name;
+    }
+
+    private  AStatUnit(String name){
+        this(name,true);
+    }
+
+    public String localized(){
+        if(name.equals(StatUnit.none.name())) return "";
+        return Core.bundle.get("unit." + name().toLowerCase(Locale.ROOT));
+    }
+}
---
+++ core/src/acontent/world/meta/AStatValues.java
@@ -0,0 +1,23 @@
+package acontent.world.meta;
+
+import arc.util.Strings;
+import mindustry.world.meta.StatUnit;
+import mindustry.world.meta.StatValue;
+import mindustry.world.meta.StatValues;
+
+public class AStatValues extends StatValues {
+
+    public static StatValue number(float value, StatUnit statUnit){
+        AStatUnit unit=AStatUnit.get(statUnit);
+        return number(value,unit);
+    }
+
+    private static StatValue number(float value, AStatUnit unit) {
+        return table -> {
+            int precision = Math.abs((int)value - value) <= 0.001f ? 0 : Math.abs((int)(value * 10) - value * 10) <= 0.001f ? 1 : 2;
+
+            table.add(Strings.fixed(value, precision));
+            table.add((unit.space ? " " : "") + unit.localized());
+        };
+    }
+}
